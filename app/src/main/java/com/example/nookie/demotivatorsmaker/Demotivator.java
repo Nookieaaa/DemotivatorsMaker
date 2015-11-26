2659dac21c8e68bd82c3454925d57aad400f789d
nookie <nookieaaa@hotmail.com>
2015-11-26 16:22:39 +0200

    2611

--- app/src/main/java/com/example/nookie/demotivatorsmaker/Demotivator.java
+++ app/src/main/java/com/example/nookie/demotivatorsmaker/Demotivator.java
@@ -6,6 +6,15 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Environment;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Demotivator {
     private Bitmap image;
@@ -24,8 +33,11 @@ public class Demotivator {
         this.text = text;
     }
 
-    public Bitmap toBitmap(){
-        Bitmap output = Bitmap.createBitmap(DEFAULT_WIDTH,DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
+    public Bitmap toBitmap() {
+        caption = "caption";
+        text = "text";
+
+        Bitmap output = Bitmap.createBitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT, Bitmap.Config.ARGB_8888);
         Bitmap scaledImage = scaleImage(image);
         Canvas c = new Canvas(output);
         if (paint==null)
@@ -33,11 +45,22 @@ public class Demotivator {
         //background
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.FILL);
-        c.drawPaint(paint);
+        //c.drawPaint(paint);
 
-        c.drawBitmap(scaledImage,(float)dpToPx(X_PADDING_DP),(float)dpToPx(Y_PADDING_DP),paint);
+        //c.drawBitmap(scaledImage,output.getWidth()/2-scaledImage.getWidth()/2,(float)dpToPx(Y_PADDING_DP),paint);
 
+        c = placeText(c);
 
+        File f = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"testdem.jpg");
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            output.compress(Bitmap.CompressFormat.JPEG,100,fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return output;
     }
@@ -47,7 +70,7 @@ public class Demotivator {
         int originalH = image.getHeight();
 
         int width = DEFAULT_WIDTH - dpToPx(X_PADDING_DP)*2;
-        int height = DEFAULT_HEIGHT - dpToPx(Y_PADDING_DP)*2;
+        int height = DEFAULT_HEIGHT - dpToPx(Y_PADDING_DP)*3;
 
         float ratio = originalW / (float) originalH;
 
@@ -68,4 +91,51 @@ public class Demotivator {
     private int dpToPx(int dp){
         return (int)(dp * Resources.getSystem().getDisplayMetrics().density);
     }
+
+
+    public String getText() {
+        return text;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    private boolean hasText(){
+        return getText().length()>0;
+    }
+
+    private boolean hasCaption(){
+        return getCaption().length()>0;
+    }
+
+    private Canvas placeText(Canvas canvas){
+        if (hasText()) {
+            canvas.translate(100,50);
+            TextPaint smallTP = new TextPaint();
+            smallTP.setStyle(Paint.Style.STROKE);
+            smallTP.setColor(Color.YELLOW);
+            smallTP.setTextSize(10);
+            StaticLayout smallTextSL = new StaticLayout(getText(),
+                    smallTP,0, Layout.Alignment.ALIGN_CENTER,0,0,false);
+            smallTextSL.draw(canvas);
+            canvas.save();
+            canvas.restore();
+        }
+        if (hasCaption()) {
+            canvas.translate(200,50);
+            TextPaint captionTP = new TextPaint();
+            captionTP.setStyle(Paint.Style.STROKE);
+            captionTP.setColor(Color.YELLOW);
+            captionTP.setTextSize(20);
+            StaticLayout captionSL = new StaticLayout(getText(),
+                    captionTP,0, Layout.Alignment.ALIGN_CENTER,0,0,false);
+            captionSL.draw(canvas);
+            canvas.save();
+            canvas.restore();
+        }
+
+        return canvas;
+    }
+
 }
