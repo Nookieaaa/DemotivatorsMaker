b733b2fc80dfd6ae76f0e2b0f023e797663a78f7
Anton <nookieaaa@hotmail.com>
2015-12-16 23:27:53 +0200

    1612

---
+++ app/src/main/java/com/nookdev/maker/dem/fragments/PreviewFragment.java
@@ -0,0 +1,21 @@
+package com.nookdev.maker.dem.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nookdev.maker.dem.R;
+
+public class PreviewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.preview_fragment,container,false);
+        return v;
+    }
+}
