236501a8171d77036cd3d6c4b4707e4acaa9fbd9
Anton <nookieaaa@gmail.com>
2018-10-08 21:13:01 +0300

    Some structuring Dependencies updated

--- app/src/main/kotlin/com/nookdev/maker/dem/view/MainActivity.kt
+++ app/src/main/kotlin/com/nookdev/maker/dem/view/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.nookdev.maker.dem
+package com.nookdev.maker.dem.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nookdev.maker.dem.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/kotlin/com/nookdev/maker/dem/view/SimpleConstructorFragment.kt
@@ -0,0 +1,5 @@
+package com.nookdev.maker.dem.view
+
+import androidx.fragment.app.Fragment
+
+class SimpleConstructorFragment : Fragment()
